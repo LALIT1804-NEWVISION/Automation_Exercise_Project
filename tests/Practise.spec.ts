94e656a82ac32cf46479abba95244c153f8373dc
LALIT1804-NEWVISION <lalit.gupta@newvision-software.com>
2026-04-24 11:09:34 +0530

    Practise updated

--- tests/Practise.spec.ts
+++ tests/Practise.spec.ts
@@ -2,6 +2,8 @@ import { test, expect, Locator } from '@playwright/test';
 //Text Box / Input Box / Text Field / Input Value / Text Area / Text Area Value
 //Checkbox / Check Box / Tick Box / Toggle Button / Switch Button
 
+test.describe('Practise Tests', () => {
+
 test('Handling HTML elements', async ({ page }) => {
 
     await page.goto('https://testautomationpractice.blogspot.com/');
@@ -28,7 +30,6 @@ test('Handling HTML elements', async ({ page }) => {
     await textBox2.fill(email);
     const maxLength2: string | null = await textBox2.getAttribute('maxlength');
     expect(maxLength2).toBe('25');
-    await page.waitForTimeout(3000);
 
     //Phone
 
@@ -39,14 +40,38 @@ test('Handling HTML elements', async ({ page }) => {
     await textBox3.fill(phone);
     const maxLength3: string | null = await textBox3.getAttribute('maxlength');
     expect(maxLength3).toBe('10');
-    await page.waitForTimeout(3000);
 
     //Address
-    const textBox4: Locator = page.locator('#address');
+    const textBox4: Locator = page.locator('//label[normalize-space()="Address:"]');
     const address = '207, Danish Nagar Rd,Rajat Bihar Colony, Bagmugaliya,Bhopal';
     await textBox4.fill(address);
-    const maxLength4: string | null = await textBox4.getAttribute('maxlength');
-    expect(maxLength4).toBe('200');
     await page.waitForTimeout(3000);
+});
+
+test('Radio Button Actions', async ({ page }) => {
+
+    await page.goto('https://testautomationpractice.blogspot.com/');
+    const male_radio_button: Locator = page.locator('#male');
+    await expect(male_radio_button).toBeVisible();
+    await expect(male_radio_button).toBeEnabled();
+    await male_radio_button.check();
+    await expect(male_radio_button).toBeChecked();
+    expect(await male_radio_button.isChecked()).toBe(true);
+    expect(await male_radio_button.isChecked()).toBeTruthy();
+
+});
+
+test('CheckBox Actions', async ({ page }) => {
+
+    await page.goto('https://testautomationpractice.blogspot.com/');
+    const Checkbox: Locator = page.locator('#male');
+    await expect(Checkbox).toBeVisible();
+    await expect(Checkbox).toBeEnabled();
+    await Checkbox.check();
+    await expect(Checkbox).toBeChecked();
+    expect(await Checkbox.isChecked()).toBe(true);
+    expect(await Checkbox.isChecked()).toBeTruthy();
+
+});
 
 });
